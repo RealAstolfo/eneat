79925135da5112f0c800baa7f9116cce69ec8f22
dots <dots@estous.net>
2026-01-03 14:49:07 -0700

    docs

--- flake.nix
+++ flake.nix
@@ -19,12 +19,14 @@
             pkg-config
             raylib
             valgrind
+            d2
           ];
 
           shellHook = ''
             echo "ENEAT development environment"
             echo "  Build: make all"
             echo "  Run:   ./neat_coro"
+            echo "  Docs:  nix build .#docs"
           '';
         };
 
@@ -66,6 +68,39 @@
             mkdir -p $out/bin
             cp neat neat_coro $out/bin/
           '';
+        };
+
+        # Documentation package - builds D2 diagrams to SVG
+        packages.docs = pkgs.stdenv.mkDerivation {
+          pname = "eneat-docs";
+          version = "0.1.0";
+          src = ./docs;
+
+          nativeBuildInputs = with pkgs; [ d2 ];
+
+          buildPhase = ''
+            mkdir -p out
+
+            # Build all D2 diagrams to SVG
+            for f in *.d2; do
+              if [ "$f" != "theme.d2" ]; then
+                name="''${f%.d2}"
+                echo "Building $name.svg..."
+                d2 --theme 200 "$f" "out/$name.svg"
+              fi
+            done
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp out/*.svg $out/
+            cp *.d2 $out/
+          '';
+
+          meta = with pkgs.lib; {
+            description = "ENEAT architecture documentation diagrams";
+            license = licenses.mit;
+          };
         };
       }
     );
